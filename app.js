891db9560bc0eb4ba9f38c0bfbab0fb4d7e9e98b
서상진 <seosangjin@seosangjin-ui-MacBookPro.local>
2020-05-02 00:39:23 +0900

    add

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const fs = require('fs');
+const router = require('./route/main')(app,fs);
 
-app.get('/', (req, res) => res.send('hello world'))
+app.set('viws', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+var server = app.listen(3000, function(){
+    console.log("Express server has started on port 3000")
+});
+
+app.use(express.static('public'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(session({
+    secret : '@#@$MYSIGN#@$#$',
+    resave : false,
+    saveUninitialized : true
+}))
+
