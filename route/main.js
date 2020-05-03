f37b80c83f0354953492b8be83e85c61812cc3ea
서상진 <seosangjin@seosangjin-ui-MacBookPro.local>
2020-05-03 14:06:13 +0900

    modify todolist url

--- route/main.js
+++ route/main.js
@@ -2,12 +2,8 @@ const express = require('express');
 const router = express();
 const todolist = require('../todolist')
 
-var server = router.listen(3000, function(){
-    console.log("Express server has started on port 3000")
+var server = router.listen(8000, function(){
+    console.log("Express server has started on port 8000")
 });
 
-router.get('/', (req, res) => {
-    res.json({'asdf' : 'hi'})
-})
-
-router.get('/todolist', todolist.excute)
+router.get('/api/todolist', todolist.excute)
